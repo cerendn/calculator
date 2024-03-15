5b772440626ee48e1872190f3ae3c77d8d246c0b
cerendn <cerendncer@outlook.com>
2024-03-15 21:09:20 +0300

    upd2

--- script.js
+++ script.js
@@ -14,10 +14,27 @@ keys.addEventListener('click', function (e) {
 
     if (!element.matches('button')) return;
 
-    if(element.classList.contains('operator')) {
-        console.log('operator' , element.value);
+    if (element.classList.contains('operator')) {
+        console.log('operator', element.value);
         return;
     }
 
-    console.log('number',element.value);
+    if (element.classList.contains('decimal')) {
+        console.log('decimal', element.value);
+        return;
+    }
+
+    if (element.classList.contains('clear')) {
+        console.log('clear', element.value);
+        return;
+    }
+
+
+    console.log('number', element.value);
+    inputNumber(element.value);
+    updateValue();
 });
+
+function inputNumber(num) {
+displayValue = display === '0' ? num: displayValue + num;
+}
